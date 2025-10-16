8e3cb2cd9c2d79d724e5641d6c4ec6d6801a7a03
Taufik Hidayat <yumtaufikhidayat@gmail.com>
2025-10-16 08:12:34 +0700

    feat: Update application version
    - Bumps the `versionCode` from 1 to 100.
    - Updates the `versionName` from "1.0" to "1.0.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "id.yumtaufikhidayat.applymate"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 100
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
